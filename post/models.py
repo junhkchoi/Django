381151e7adcfad33a4f9d5d9c6990814fbde34bb
junhkchoi <leisure1566@gmail.com>
2025-03-10 21:28:21 +0900

    feat: CRUD 세팅 2

--- post/models.py
+++ post/models.py
@@ -6,4 +6,7 @@ class Post(models.Model):
 
     title = models.CharField(max_length=20)
     content = models.TextField()
-    create_at = models.DateTimeField('auto_now')
+    create_at = models.DateTimeField(auto_now_add=True) 
+
+    def __str__(self):
+        return self.title
--- post/views.py
+++ post/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
-# Create your views here.
+from . models import Post
 
+# 공지사항 목록
 def post_list(request):
     return render(request, 'post/post_list.html')
 
-# def detail_post(request):
-#     return render(request, 'post/detail_post.html')
+# CRUD
+def detail_post(request):
+    return render(request, 'post/detail_post.html')
 
+def create_post(request):
+    if request.method == 'POST':
+        post = Post.objects.create(
+            title = request.POST['title'],
+            content = request.POST['content']
+        )
+        return redirect('post_list')
+    return render(request, 'post/create_post.html')
+
+def update_post(request):
+    return render(request, 'post/update_post.html')
+
+def delete_post(request):
+    return render(request, 'post/delete_post.html')
